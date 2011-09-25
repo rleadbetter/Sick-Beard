2083ffe5b23c814055a7b6d75a5aa1561f804429
Taylor Raack <traack@raack.info>
2011-09-25 09:58:41 -0400

    Added KickAssTorrent provider

--- sickbeard/webserve.py
+++ sickbeard/webserve.py
@@ -1084,6 +1084,8 @@ class ConfigProviders:
                 sickbeard.WOMBLE = curEnabled
             elif curProvider == 'ezrss':
                 sickbeard.EZRSS = curEnabled
+            elif curProvider == 'kat':
+                sickbeard.KAT = curEnabled
             elif curProvider == 'tvtorrents':
                 sickbeard.TVTORRENTS = curEnabled
             elif curProvider in newznabProviderDict:
