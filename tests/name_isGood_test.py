4fa7b2183a80b4096923e3d0039744245354b8e7
Dennis Lutter <lad1337@gmail.com>
2012-04-24 23:34:25 +0200

    improve/fix all_test.py and other improvements to the unites like removed unneeded imports or test_lib debug messages and refactored "test method creation" functions add name_isGood tests

---
+++ tests/name_isGood_test.py
@@ -0,0 +1,65 @@
+
+import unittest
+import test_lib as test
+
+from name_parser_tests import simple_test_cases
+import sickbeard.show_name_helpers
+from sickbeard.tv import TVShow
+
+
+dontTest = ["Show.Name.102",
+            "Show.Name.102.Source.Quality.Etc-Group",
+            "show.name.2010.123.source.quality.etc-group",
+            "show.name.2010.222.123.source.quality.etc-group",
+            "the.event.401.hdtv-lol",
+            "Show_Name.1x02.Source_Quality_Etc-Group",
+            "Show_Name.1x02x03x04.Source_Quality_Etc-Group",
+            "Show Name - 01 - Ep Name",
+            "Show Name - Episode 01 - Ep Name",
+            "Show Name - Episode 01-02 - Ep Name",
+            "Show Name Season 2"]
+
+
+class TestSequense(test.SickbeardTestDBCase):
+    pass
+
+def test_generator(toTest, tvdb_id, parseResult):
+    def test(self):
+        if parseResult.series_name:
+            show = TVShow(parseResult.series_name)
+            show.name = parseResult.series_name
+
+            show.saveToDB()
+            isGood = sickbeard.show_name_helpers.isGoodResult(sickbeard.helpers.sanitizeSceneName(toTest), show)
+            self.assertTrue(isGood, "'" + toTest + "' it is not a valid result '" + show.name + "'")
+        self.assertTrue(True)
+    return test
+
+
+tvdb_id = 1
+for t in simple_test_cases:
+    for cur_test_base in simple_test_cases[t]:
+        if cur_test_base in dontTest:
+            continue
+        saneBase = sickbeard.helpers.sanitizeSceneName(cur_test_base)
+
+        test_name = 'test_%s_%s' % (t, cur_test_base)
+
+        if not simple_test_cases[t][cur_test_base]:
+            continue
+
+        testFunc = test_generator(cur_test_base, tvdb_id, simple_test_cases[t][cur_test_base])
+        setattr(TestSequense, test_name, testFunc)
+        tvdb_id += 1
+
+
+
+if __name__ == '__main__':
+    print
+    print "#####################"
+    print "Running isGood result test now"
+    print "#####################"
+
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestSequense)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+
